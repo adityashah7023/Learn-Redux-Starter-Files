ce435913e0ef29b812e7887b70496f41bb38f69b
Aditya Shah <aditya.shah7023@gmail.com>
2019-01-02 00:54:21 -0500

    Added React Router

--- learn-redux/client/components/Main.js
+++ learn-redux/client/components/Main.js
@@ -7,6 +7,10 @@ const Main = React.createClass({
       <div>
         <h1>
           <Link to="/">Reduxstagram</Link>
+          {
+            React.cloneElement(this.props.children, this.props)
+          }
+          
         </h1>
       </div>
     );
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Main from './components/Main';
 
+//Import Components
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+//import Styles
 import css from './styles/style.styl';
 
-render(<Main/>, document.getElementById('root'));
+//import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+)
+
+render(router, document.getElementById('root'));
